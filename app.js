230cd38070bc491aa3c47df1cadbfd8ad1e06aa5
rkmehta24 <rkmehta246@gmail.com>
2022-07-30 01:51:03 +0530

    Update app.js for heroku

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.post("/work",function(req,res) {
     res.redirect("/work");
 })
 
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT, function() {
     console.log("Server is running on port 3000.");
 });
